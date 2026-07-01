9703e25f7f94bb8c7875dec8f70d49fe27396b03
stuchihayt-creator <stuchihayt@gmail.com>
2026-07-01 16:52:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("MineWeb Loaded!");
+// Username System
+
+let username = localStorage.getItem("mineweb_username");
+
+if (!username) {
+    setTimeout(() => {
+        let name = prompt("👋 Welcome to MineWeb!\n\nEnter your username:");
+
+        while (!name || name.trim() === "") {
+            name = prompt("Username cannot be empty!");
+        }
+
+        localStorage.setItem("mineweb_username", name.trim());
+
+        alert("Welcome " + name + "!");
+        location.reload();
+    }, 500);
+} else {
+    console.log("Logged in as: " + username);
+}
