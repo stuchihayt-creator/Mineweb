699141b674863cd454fd1d96c4bd357fe1499755
stuchihayt-creator <stuchihayt@gmail.com>
2026-07-02 16:32:03 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxWpiz4kJCkuChJckX8e4fg04FI8A6qNs",
+  authDomain: "mineweb-7ba71.firebaseapp.com",
+  projectId: "mineweb-7ba71",
+  storageBucket: "mineweb-7ba71.firebasestorage.app",
+  messagingSenderId: "359816018394",
+  appId: "1:359816018394:web:2fe1856cc6b72e77c828e3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
