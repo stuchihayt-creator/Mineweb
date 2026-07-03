4fd24eb7232477a07c1a70214f3013c64375f07b
stuchihayt-creator <stuchihayt@gmail.com>
2026-07-03 15:50:49 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,46 @@
+import { db } from "./firebase.js";
+
+import {
+collection,
+getDocs,
+query,
+orderBy
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const postsDiv = document.getElementById("posts");
+
+async function loadPosts(){
+
+const q = query(collection(db,"posts"),orderBy("createdAt","desc"));
+
+const snap = await getDocs(q);
+
+postsDiv.innerHTML="";
+
+snap.forEach(doc=>{
+
+const p=doc.data();
+
+postsDiv.innerHTML+=`
+
+<div class="card">
+
+<h2>${p.title}</h2>
+
+<p>${p.description}</p>
+
+<p><b>Category:</b> ${p.category}</p>
+
+<p><b>Version:</b> ${p.version}</p>
+
+<p>👤 ${p.username}</p>
+
+</div>
+
+`;
+
+});
+
+}
+
+loadPosts();
